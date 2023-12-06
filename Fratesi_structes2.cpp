ce04a551f8aed32bd24123cd327b992b85162554
jacopofratesi <147489382+jacopofratesi@users.noreply.github.com>
2023-12-06 18:16:59 +0100

    Create Fratesi_structes2.cpp

---
+++ Fratesi_structes2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+#define N 4
+#include <array>
+struct livello{
+    char livlet;
+    int livn;
+};
+struct sezione{
+    int anno;
+    char sez;
+};
+struct studente {
+   int nome ;
+    
+    livello livellostudente;
+    sezione sezionestudente;
+    
+};
+int main() {
+    int l;
+    studente array[N];
+    for( int i=0; i<N;i++){
+        cout<<"codice studente "<<i<<endl;
+        cin>>array[i].nome;
+       cout<<"sezione studente "<<i<<endl;
+        cin>>array[i].sezionestudente.anno;
+        cin>>array[i].sezionestudente.sez;
+        cout<<"livello studente "<<i<<endl;
+        cin>>array[i].livellostudente.livlet;
+        cin>>array[i].livellostudente.livn;
+        
+    }
+    
+    for( int i=0; i<N;i++){
+        for(int j=0;j<N;j++){
+            if(array[i].sezionestudente.anno < array[j].sezionestudente.anno)
+            {
+                if(array[i].livellostudente.livlet < array[j].livellostudente.livlet)
+                {
+                    cout<<"studente "<<i<< "più piccolo ma livello più grande"<<endl;
+                    
+                }
+                if((array[i].livellostudente.livn == array[j].livellostudente.livn) && array[i].livellostudente.livn == array[j].livellostudente.livn )
+                {
+                     cout<<"codice studente "<<i<<endl;
+        cin>>array[i].nome;
+       cout<<"sezione studente "<<i<<endl;
+        cout<<array[i].sezionestudente.anno;
+        cout<<array[i].sezionestudente.sez;
+        cout<<"livello studente "<<i<<endl;
+        cout<<array[i].livellostudente.livlet;
+        cout<<array[i].livellostudente.livn; 
+                }
+            }
+        }
+    }
+    
+    
+    return 0;
+}
