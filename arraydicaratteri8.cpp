5365dd24e97facca1be13fc3fee78fd4a65e2783
jacopofratesi <147489382+jacopofratesi@users.noreply.github.com>
2023-12-21 21:30:58 +0100

    Update arraydicaratteri8.cpp

--- arraydicaratteri8.cpp
+++ arraydicaratteri8.cpp
@@ -14,7 +14,8 @@ main ()
   int i = 0;
   int j = 0;
   int n = 0;
-  int c=0;
+  int c = 0;
+ int max = 0;
   char video[N];
   while (stringa[i] != '\0')
     {
@@ -26,21 +27,25 @@ main ()
      
     }
    for(a=0;a<i;a++){
-       
+       n=0;
        for (j=0;j<i;j++){
-          
+          //verifico se ci sono caratteri simili
            if(stringa[j]==stringa[a]){
            video[n]=stringa[j];
-           c++;
-        n++;
+          c++;
+          n++;
+          //trovo carattere massimo
+          if(c>max){
+          max=c;
+          
+          }
+          
            } 
        }
         
    }
- cout<<"la lettera ripetuta più volte è: "<<endl;
- for(a=0;a<n;a++){
-     if(video[a]>video[a+1])
-     cout<<video[a]<<endl;
- }
+ 
+ cout<<"viene ripetuta più volte la lettera: "<<video[n]<<endl;
+
   return 0;
 }
