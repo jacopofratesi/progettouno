ba671640914eb1efe10de29dc900347d981481fd
jacopofratesi <147489382+jacopofratesi@users.noreply.github.com>
2024-01-08 10:16:24 +0100

    Create jarraydicaratteri8.cpp

---
+++ jarraydicaratteri8.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#define N 30
+using namespace std;
+
+int
+main ()
+{
+
+  char stringa[N];
+  
+  cout<<"Inserisci una stringa:"<<endl;
+  cin >> stringa;
+  int a = 0;
+  int i = 0;
+  int j = 0;
+  int n = 0;
+  int c = 0;
+ int max = 0;
+  char video[N];
+  while (stringa[i] != '\0')
+    {
+       
+       
+        
+      i++;
+     
+     for(a=0;a<i;a++){
+       n=0;
+       for (j=0;j<i;j++){
+          //verifico se ci sono caratteri simili
+           if(stringa[j]==stringa[a]){
+           
+          
+          n++;
+          //trovo carattere massimo
+          if(n>max){
+          max=c;
+          video[n]=stringa[max];
+          }
+          
+           } 
+       }
+        
+   }
+    }
+   
+ 
+ cout<<"viene ripetuta più volte la lettera: "<<video[n]<<endl;
+
+  return 0;
+}
