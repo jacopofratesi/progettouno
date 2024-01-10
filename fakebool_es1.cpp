336471b1d818906065940fb5abd58fb6c97b24f0
jacopofratesi <147489382+jacopofratesi@users.noreply.github.com>
2024-01-10 13:28:34 +0100

    Create fakebool_es1.cpp

---
+++ fakebool_es1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void calcolanome(char x[];char y[]; int z;int i;int a1;int a2)
+//x == nome, y ==cognome, z|i == contatori, a1|a2 == indici
+{
+for(int i = 0;i<a;i++){
+if(x||"A" && x||"E" && x||"O" && x||U && x||"I")
+z++;
+}
+for(int i = 0;i<a;i++){
+if(x=="A" && x=="E" && x=="O" && x==U && x=="I")
+i++;
+}
+}
+int main(){
+int l1 = 0;
+int l2 = 0;
+char nome[258];
+cin>>nome;
