46a46f4fdfef3ae91dfa338958adec4bfca47569
jacopofratesi <147489382+jacopofratesi@users.noreply.github.com>
2024-02-01 10:18:05 +0100

    Create stringa_es1.cpp

---
+++ stringa_es1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+  string parola;
+   cin>>parola;
+ 
+   cout<<"lunghezza: "<< parola.length()<<endl;
+   cout<<parola<<endl;
+    return 0;
+}
