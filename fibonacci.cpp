d9268303cabbdae14305ef9dc2baea7d74451933
jacopofratesi <147489382+jacopofratesi@users.noreply.github.com>
2024-02-06 15:23:15 +0100

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 using namespace std;
 int fat_iter(int n)
-{
-  int i = n;
-   int ris =n;
-   
-    do{
-    i--;
-     ris = ris*i;
-     
-    }while(i!=1);
-    return(ris);
-}
-int fat_ric(int n){
- if(n==0)
- return 1;
- return n*fat_iter(n-1);
+
 }
 int fib(int n){
     if (n==0)
