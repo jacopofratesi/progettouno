158c91a3f641df15cd8868888fcca2926921e220
jacopofratesi <147489382+jacopofratesi@users.noreply.github.com>
2024-03-04 08:49:17 +0100

    Create unione.cpp

---
+++ unione.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int
+main ()
+{
+  ifstream primo ("primo.txt");
+  ifstream secondo ("secondo.txt");
+  ofstream unito ("unito.txt");
+  string a, b;
+  primo >> a;
+  secondo >> b;
+  unito << a + b << endl;
+  primo.close ();
+  secondo.close ();
+  for (int i = 0; i < a.length (); i++)
+	a[i] = toupper (a[i]);
+  for (int i = 0; i < b.length (); i++)
+	b[i] = toupper (b[i]);
+  unito << a + b << endl;
+  return 0;
+}
